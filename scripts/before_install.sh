bd33f11ff07fc4c83df1fbab22b9f57a4ec20678
Josh Krebs <joddykrebs4@gmail.com>
2023-03-06 16:52:49 -0800

    changed to nmv install 16

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,7 +3,7 @@
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install --lts
+nvm install 16
 
 
 #create our working directory if it doesnt exist
