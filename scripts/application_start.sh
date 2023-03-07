29209f4ef07cce95c2faa0ce6d568fe0f5908b11
Josh Krebs <joddykrebs4@gmail.com>
2023-03-06 17:14:05 -0800

    tryin again

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,6 +12,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
+cd
+cd express-app
 npm install
 
 #start our node app in the background
